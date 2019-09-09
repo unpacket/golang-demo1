9ee0a84ebd08456f2e35836a3eed6fa2401d6568
unpacket <unpacket@gmail.com>
2019-09-09 11:03:29 -0700

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export UUID=$(uuidgen)
 
 make skaffold-run
-reflex -r "\.go$" -R "vendor.*" make skaffold-run
+reflex -r "\.go$" -R "vendor.*" -- bash -c 'export UUID=$(uuidgen) && make skaffold-run
